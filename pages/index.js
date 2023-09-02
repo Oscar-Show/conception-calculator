639d0d446fde0779eb2f915a336885d564940c0d
Oscar-Show <brahim.ecom1990@gmail.com>
2023-09-02 14:13:12 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,7 @@ import styles from '../styles/global.css'; // Adjust the path based on your proj
 
 const Home = () => {
 
-  useEffect(() => {
-    // Google Analytics tracking code
-    window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-83T28LQK4Q');
-  }, []);
+  
   return (
     <div>
       <Header />
